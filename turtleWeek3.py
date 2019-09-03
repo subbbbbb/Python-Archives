2e8b410484146b428771c65fabe66bb5a19d623c
submohanty <submohanty21@gmail.com>
2019-09-03 09:40:37 -0600

    Added the polygon maker method to the turtleWeek3.py

--- turtleWeek3.py
+++ turtleWeek3.py
@@ -1,3 +1,4 @@
+"""
 import turtle
 sam = turtle.Turtle()
 sam.penup()
@@ -7,3 +8,15 @@ for i in range(4):
     sam.forward(50)
     sam.right(90)
 turtle.done()
+"""
+import turtle
+sam = turtle.Turtle()
+myscreen = turtle.Screen()
+
+def regularpolygon(sides):
+    angle = 360/sides
+    for i in range(sides):
+        sam.forward(50)
+        sam.right(angle)
+regularpolygon(3)
+regularpolygon(5)
