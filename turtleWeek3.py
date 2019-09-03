f1e4c855295a8779ed87e62ebd718df054620e0a
submohanty <submohanty21@gmail.com>
2019-09-03 09:11:02 -0600

    Added the setposition line of code

--- turtleWeek3.py
+++ turtleWeek3.py
@@ -1,6 +1,9 @@
 import turtle
 sam = turtle.Turtle()
+sam.penup()
+sam.setposition(-400,400)
+sam.pendown()
 for i in range(4):
     sam.forward(50)
-    sam.right(50)
+    sam.right(90)
 turtle.done()
