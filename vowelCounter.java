6d51c32c5b56604099b2ee9594091032c089d663
sub_mohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-13 03:54:32 -0600

    Made the necesary change to add vowelCounter's class name

--- vowelCounter.java
+++ vowelCounter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class vowelCounter {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your word and I'll tell you how many vowels there are::");
