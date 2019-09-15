a1ed313eac700f206c261f21f71023e28db2293a
sub_mohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-14 18:20:31 -0600

    Removed the old caesarCyper and added the new one

--- caesarCypher.py
+++ caesarCypher.py
@@ -1,44 +1,52 @@
-# The premise of the code is that you want to encode/decode the message 
-# 1 letter to the right. 
-# For example
+'''
+def getMode(): #defining mode
+    
+    mode =  input("you wanna encrypt or decrypt dummy?").lower()#convert single letters to lowercase
+    while True: #loop
+        if mode == "encrypt" or mode == "decrypt": #saying what the user will do 
 
-
-"""Define a function to ask user if they want to encrypt or decrypt and return the answer"""
-def getMode(): # empty paramter list
-    mode = input("Do you wish to encrypt or decrypt?").lower()
-    while True:
-        mode = input("Do you wish to to encrypt or decrypt?").lower()
-        if mode == "encrypt" or mode == "decrypt":
-            return mode
+            return mode #returning mode
         else:
-           mode = input("Do you wish to to encrypt or decrypt?").lower()
-# print(getMode())
-
-
-def getMessage():
-    return input("What is your message?")
-# print(getMessage())
+            mode = input("Spell it right and stop wasting my time").lower() #spell it right
+            
+print(getMode())#print it
 
 def getMessage():
-    return input("What is your message?")
-# print(getMessage())
+    return input("Encrypt or Decrypt")
+print(getMessage())
 
-def getKey(): # gets the key for the cipher
-    key = int(input("Enter a key between 1 and 25"))
+def getKey():
+    try:
+        key = int(input("enter key between 1-25"))
+        return key
+    except:
+        key = int(input("Enter key between 1-25"))
+    
+
+
+
+
+    
+def getKey ():
+    key = int(input("Enter a number betwen -25 and 25")) #asking for a key and convert to int
     while True:
-        if key >= -25 and key <= 25:
+        if key >=-25 and key<=25:
             return key
-        else:
-            key = int(input("Enter a key between 1 and 25"))
+    else:
+        key = int(input("I need a number 1-25"))
+print(getKey())
+'''
 
-def getTranslatedMessage(mode, message, key): # parameter list
-    if mode[0] == "d": # if first letter in mode variable is d
-        key = -key # switch key for decryption
-
-    translated = "" # variable for storing translated message
-    for letter in message: # looks at each letter in message 
+def getTranslatedMessage(mode, message, key): #parameter list
+    if mode[0] == 'd': #if first letter in mode variable is d grab part of string
+        key = -key #switch key for decryption
+    translated = ""
+    for letter in message:
         num = ord(letter)
-        num += key 
+        num = num +key
         translated += chr(num)
-
-    return translated
+    return translated 
+print(getTranslatedMessage("encrypt", "Hello", 1))
+print(getTranslatedMessage("decrypt", "Ifmmp", 1))
+print(getTranslatedMessage("encrypt", "Sub", 2))
+print(getTranslatedMessage("decrypt", "Uwd", 2)) # the number says how many letters to move to the right
---
+++ caesarCypherAtHome.py
@@ -0,0 +1,6 @@
+def getTranslatedMessage(mode,message,key):
+    # mode is either encrypt or decrypt
+    # message is the word you want translated
+    # key is the number of spaces moved to the left or right
+    
+
