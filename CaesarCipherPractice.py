e803b43181380880240fd4b7cd5b52f07bb44e3e
sub_mohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-14 19:51:54 -0600

    Changed file names

--- CaesarCipherPractice.py
+++ CaesarCipherPractice.py
@@ -4,9 +4,9 @@ def getTranslatedMessage(mode, message, key):  # translator method
     # key is the number of spaces moved to the left or right
     # the ord() function takes a single character and gives the ascii value of it
 
-    # all this does is check if it is encrypt or decrypt, mode.substring(0,1)
-    if mode[0] == 'd':
-        key = -key  # this switches the order and makes it so the message will be decrypted
+    
+    if mode[0] == "d": # all this does is check if it is encrypt or decrypt, mode.substring(0,1)
+        key = -key  # this switches the order and makes it so you will subtract the ascii values instead of adding them
     translated = ""  # the final word that will be printed out
     for letter in message:  # letter is the individual letter in each letter of message
         num = ord(letter)  # gets the ascii value of the letter/number
@@ -15,9 +15,10 @@ def getTranslatedMessage(mode, message, key):  # translator method
         translated += chr(num)
     return translated  # need to add the return because this is inside a method
 
-    # make sure the return statement is not inside the for loop
+    # make sure the return statement is not inside the for loop, because then it would only return the first translated message then never include anything else
 
 
 # test cases
 print(getTranslatedMessage("encrypt", "helloworld!", 1))
 print(getTranslatedMessage("decrypt", "ifmmpxpsme\"", 1))
+print(getTranslatedMessage("e", "hello", 1)) # as long as the first letter is d, it will decrypt, or else it will encrypt
