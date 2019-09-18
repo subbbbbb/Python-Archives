e1eea1b2e01a59bf42cc41f5419f5f5a53588d61
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-17 21:03:36 -0600

    making some changes on sublime

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,16 +1,20 @@
-<<<<<<< HEAD
- 
-# getMode() is a getter method that either prints out decrypt/encrypt, or tells you to do it over again if invalid input
-def getMode(): #defining mode
-    mode =  input("do you want to encrypt or decrypt?").lower()#convert single letters to lowercase
-    while True: #loop
-=======
 
+# getMode() is a getter method that either prints out decrypt/encrypt, or tells you to do it over again if invalid input
+def getMode(): # defines the specific mode that you want
+    mode =  input("do you want to encrypt or decrypt?").lower() # converts single letters to lowercase to prevent problems later
+    while True: #loop
+        if(mode == "encrypt" or mode == "decrypt"):
+            return mode
+        else: 
+            print("Spell it correctly and try again")
+            mode = input("Spell it right and try again::").lower()
+print(getMode())
+
+"""
 def getMode(): # basically a getter method for getting the type you want (encrypt or decrypt)
     
     mode =  input("Would you like to encrypt or decrypt?").lower() #convert single letters to lowercase, prevents problems with ASCII
     while True: # always true, as long as the input is
->>>>>>> a6eb884b377e35498aa3940612abebfb4fc7fc21
         if mode == "encrypt" or mode == "decrypt": #saying what the user will do 
 
             return mode #returning mode
@@ -45,11 +49,6 @@ def getKey ():
         key = int(input("I need a number 1-25"))
 print(getKey())
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a6eb884b377e35498aa3940612abebfb4fc7fc21
-
 def getTranslatedMessage(mode, message, key): #parameter list
     if mode[0] == 'd': #if first letter in mode variable is d grab part of string
         key = -key #switch key for decryption
@@ -62,3 +61,4 @@ def getTranslatedMessage(mode, message, key): #parameter list
 print(getTranslatedMessage("encrypt", "Hello", 1))
 print(getTranslatedMessage("decrypt", "Ifmmp", 1))
 print(getTranslatedMessage("encrypt", "Sub", 2))
+"""
