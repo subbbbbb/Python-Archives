02142bba83b5e2ab236de0112fa6f6c8f3a3bd3f
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-19 19:13:55 -0600

    Random changes

--- testscore.py
+++ testscore.py
@@ -11,6 +11,6 @@ elif int(myGrade) > 59 and int(myGrade) < 70:
 elif int(myGrade) > 69 and int(myGrade) < 80:
     print("you got a C")
 elif int(myGrade) > 79 and int(myGrade) < 90:
-    print("damn you got an A")
+	print("damn you got an A")
 elif int(myGrade) > 100:
     print("liar")
