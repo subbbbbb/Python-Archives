aa0100acf3f9091b1561c635c0a8bd7d5d4d397f
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-21 15:20:38 -0600

    Finished up the turtleHouse, and edits to floyd

--- pinkFloyd.py
+++ pinkFloyd.py
@@ -3,14 +3,13 @@
 # All the import statements needed
 import turtle
 from turtle import *
-import random
 from random import randint
 
 # Declares the turtle screen and gives the window a name
 myScreen = turtle.Screen()
 myScreen.title("Dark Side of the Moon")
 colormode(255)
-myScreen.bgcolor(50,60,80)
+myScreen.bgcolor(50, 60, 80)
 
 # Creates a new pen object
 sub = turtle.Turtle()
@@ -20,43 +19,43 @@ sub.pensize(5)
 sub.speed("slow")
 
 # Puts the pen down and starts writing the triangle
-sub.goto(-100,0)
+sub.goto(-100, 0)
 sub.pendown()
-sub.forward(200)          
-sub.right(-120)       
-    
-sub.forward(200)          
-sub.right(-120)           
-sub.forward(200)          
+sub.forward(200)
+sub.right(-120)
+sub.forward(200)
+sub.right(-120)
+sub.forward(200)
 sub.right(-120)
 
 # Adds the Pink Floyd text
 sub.penup()
-sub.goto(0,200)
+sub.goto(0, 200)
 colormode(255)
-red1 = randint(0,255)
-blue1 = randint(0,255)
-green1 = randint(0,255)
-sub.pencolor(red1,blue1,green1) # randomizes the color of the text
-sub.write("Pink Floyd: Dark Side of the Moon", True, align= "center", font=("Arial", 24, "bold"))
+red1 = randint(0, 255)
+blue1 = randint(0, 255)
+green1 = randint(0, 255)
+sub.pencolor(red1, blue1, green1)  # randomizes the color of the text
+sub.write("Pink Floyd: Dark Side of the Moon", True, align="center", 
+    font=("Arial", 24, "bold"))
 sub.pencolor("white")
 sub.pendown()
 x = 1
-sub.goto(25,50) # Does the little line to the triangle
+sub.goto(25, 50)  # Does the little line to the triangle
 
 # Does the RGB line that is supposed to replicate a rainbow
-while x < 500: # This makes it so that 600 isoceles triangles will be printed
+while x < 500:  # This makes it so that 600 isoceles triangles will be printed
     cool = turtle.Turtle()
-    x+=1 # Every time it iterates through the while loop the triangles get bigger
-    colormode(255) # Allows you to use the 255 RGB colors ***I think***
+    x += 1  # Every time it goes through the loop, it - one from number
+    colormode(255)  # Allows you to use the 255 RGB colors ***I think***
     cool.pensize(1)
     cool.speed(100)
-    red = randint(0,255)
-    green = randint(0,255) # I would've used the randints above but that way the colors won't change
-    blue = randint(0,255)
-    cool.pencolor(red,green,blue) # this will make it some random color
-    cool.forward(40 + x) # it is 50 + x because the rainbow gets bigger as x+=1
+    red = randint(0, 255)
+    green = randint(0, 255) # I would've used the randints above but that way the colors won't change
+    blue = randint(0, 255)
+    cool.pencolor(red, green, blue)  # this will make it some random color
+    cool.forward(40 + x)  # it is 50 + x because the rainbow gets bigger as x+=1
     cool.right(200)
-    
-print("Finished.")    
+
+print("Finished.")
 sub.done()
--- turtleHouse.py
+++ turtleHouse.py
@@ -1,35 +1,74 @@
 import turtle
 from turtle import *
-import random
-from random import randint
 
+# Sets the basic turtle window
 myScreen = turtle.Screen()
 myScreen.title("House")
 colormode(255)
 myScreen.bgcolor("cyan")
 
+# Sets the pen
 sub = turtle.Turtle()
 sub.pencolor("black")
 sub.color("black")
 sub.pensize(4)
-sub.speed("slow")
+sub.speed("fastest")
 
+# Creates the house
 sub.penup()
-sub.goto(-100,0)
+sub.goto(-100, 0)
 sub.pendown()
-sub.goto(100,0)
-sub.goto(100,200)
-sub.goto(-100,200)
-sub.goto(-100,0)
+sub.goto(100, 0)
+sub.goto(100, 200)
+sub.goto(-100, 200)
+sub.goto(-100, 0)
 sub.penup()
-sub.goto(-100,200)
+
+# Creates the roof for the house
+sub.goto(-100, 200)
 sub.pendown()
-sub.goto(0,300)
-sub.goto(100,200)
+sub.goto(0, 300)
+sub.goto(100, 200)
 sub.penup()
-sub.goto(-50,0)
+
+# Creates the door for the house
+sub.goto(-25, 0)
 sub.pendown()
-sub.goto(-50,50)
-sub.goto(50,50)
-sub.goto(50,0)
+sub.goto(-25, 100)
+sub.goto(25, 100)
+sub.goto(25, 0)
+sub.penup()
+
+# Creates the window for the house
+sub.goto(-75, 100)
+sub.pendown()
+sub.goto(-75, 150)
+sub.goto(-50, 150)
+sub.goto(-50, 100)
+sub.goto(-75, 100)
+sub.penup()
+
+# Make the grass
+sub.begin_fill()
+sub.fillcolor("green")
+sub.pencolor("green")
+sub.color("green")
+sub.goto(-400, 0)
+sub.pendown()
+sub.goto(400, 0)
+sub.goto(400, -350)
+sub.goto(-400, -350)
+sub.goto(-400, 0)
+sub.penup()
+sub.end_fill()
+
+# Creates the sun
+sub.pencolor("yellow")
+sub.color("yellow")
+sub.goto(200, 200)
+sub.pendown()
+sub.begin_fill()
+sub.fillcolor("yellow")
+sub.circle(30)
+sub.end_fill()
 
