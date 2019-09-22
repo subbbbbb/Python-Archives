9bb4f5c210dc60712596f925e5876343a377b43e
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-21 23:34:50 -0600

    Added some random lua file

---
+++ helloworld.lua
@@ -0,0 +1,9 @@
+local wx = require 'wx'
+
+local PATH_TO_APPLICATION = [[notepad.exe]]     -- Windows assumed for sake of exemplification
+
+local ans = wx.wxMessageBox( "Should the application be started?", "Hi there!",
+    wx.wxOK + wx.wxCANCEL + wx.wxICON_QUESTION )
+if ans == wx.wxOK then
+    wx.wxExecute( PATH_TO_APPLICATION )
+end
