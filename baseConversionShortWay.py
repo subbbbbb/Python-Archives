15fb310f587da10b48c3aae7a321dfdf4f554729
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-23 07:05:35 -0600

    Added the baseConversionShortWay file

--- baseConversionShortWay.py
+++ baseConversionShortWay.py
@@ -1,7 +1,10 @@
-binOrDec = input("Do you want to convert bintoD or DtoBin")
+binOrDec = input("Do you want to convert bintoD or DtoBin or decToHex")
 if(binOrDec == "bintoD"):
     hi = input("Enter your binary number::")
     print(int(hi, 2))
 elif(binOrDec == "DtoBin"):
 	wassup = int(input("Enter a decimal number"))
 	print(bin(wassup).replace("0b", ""))
+elif(binOrDec == "decToHex"):
+	wassup = int(input("Enter a decimal number"))
+	print(hex(wassup).replace("0x","").upper())
