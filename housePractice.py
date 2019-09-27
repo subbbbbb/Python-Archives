a8608322cf12a4a6f9508c28c14cb6311a34e94b
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-26 23:04:58 -0600

    Finished practicing for the test on houses

--- housePractice.py
+++ housePractice.py
@@ -1,4 +1,14 @@
 import turtle
 
 myScreen = turtle.Screen()
-tur
+myScreen.bgcolor("green")
+sub = turtle.Turtle()
+sub.pencolor("white")
+sub.color("white")
+sub.speed("slow")
+sub.goto(0, 0)
+sub.goto(200, 0)
+sub.goto(200, 200)
+sub.goto(-200, 200)
+sub.goto(-200, 0)
+sub.goto(0, 0)
