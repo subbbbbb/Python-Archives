f8fefc356a830a944fb06b3e67af0adbe3efa6cd
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-09-29 09:25:19 -0600

    Added a comment

--- BashPrac.py
+++ BashPrac.py
@@ -2,11 +2,13 @@ alphabet_convert = {'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V',
         'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q', 
         'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L', 
         'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G', 
-        'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A', " ": " "} 
+        'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A', " ": " "}
+# make sure to have the curly braces around this
 print("Welcome to the atBash cipher calculator!")
 word = input(("Enter the word that you would like to encrypt")).upper()
-answer = ""
+answer = ""p
 for letter in word:
 	answer += alphabet_convert[letter]
 print(answer)
+
 
