84d1bd4e31155862fc5d6a95cc79163420972564
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-10-01 23:26:07 -0600

    Some tracked changes

--- BashPrac.py
+++ BashPrac.py
@@ -1,14 +1,15 @@
-alphabet_convert = {'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V', 
-        'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q', 
-        'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L', 
-        'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G', 
-        'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A', " ": " "}
+alphabet_convert = {'A': 'Z', 'B': 'Y', 'C': 'X', 'D': 'W', 'E': 'V',
+                    'F': 'U', 'G': 'T', 'H': 'S', 'I': 'R', 'J': 'Q',
+                    'K': 'P', 'L': 'O', 'M': 'N', 'N': 'M', 'O': 'L',
+                    'P': 'K', 'Q': 'J', 'R': 'I', 'S': 'H', 'T': 'G',
+                    'U': 'F', 'V': 'E', 'W': 'D', 'X': 'C', 'Y': 'B', 'Z': 'A', " ": " "}
 # make sure to have the curly braces around this
 print("Welcome to the atBash cipher calculator!")
 word = input(("Enter the word that you would like to encrypt")).upper()
-answer = ""p
+answer = ""
 for letter in word:
-	answer += alphabet_convert[letter]
+    answer += alphabet_convert[letter]
+print("Your original word:")
+print(word)
+print("Your translated message:")
 print(answer)
-
-
--- getMessage.py
+++ getMessage.py
@@ -1,3 +1,5 @@
 def getMessage():
-	return input("What is the message that you want to encrypt/decrypt?").lower()
+    return input("What is the message that you want to encrypt/decrypt?").lower()
+
+
 print(getMessage())
