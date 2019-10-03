b71eb4cf7ef67396c22d8a84427bfef2d9e9289b
submohanty <48540588+submohanty@users.noreplay.github.com>
2019-10-02 23:02:22 -0600

    Pep8 formatting

--- classwork.py
+++ classwork.py
@@ -2,11 +2,11 @@ x = int(input())
 y = float(input())
 z = str(input())
 
-print(float(x)*y)
+print(float(x) * y)
 print(len(z))
 
-print(x%2)
+print(x % 2)
 
-print(y%2)
+print(y % 2)
 
-print(float(len(z))/2)
+print(float(len(z)) / 2)
