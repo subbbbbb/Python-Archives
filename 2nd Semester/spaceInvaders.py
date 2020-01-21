7a1fe6d989e3ce53ee4c7b9a59ebb25e6b9728ce
submohanty <submohanty21@gmail.com>
2020-01-21 09:46:11 -0700

    Key changes from arrow keys to wasd

--- 2nd Semester/spaceInvaders.py
+++ 2nd Semester/spaceInvaders.py
@@ -50,7 +50,7 @@ class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
